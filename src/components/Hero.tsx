119cc8f6d9bff24f73211edf0da0881ef5ae4f16
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-09-15 22:54:08 +0200

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
         <h2 className="text-center text-white uppercase pt-14 text-1xl xl:text-4xl font-stapel ">
           Your Ultimate Minecraft Hosting <br /> Solution
         </h2>
-        <div className="flex flex-col items-center pt-6 gap-3 xl:flex-row xl:flex ">
+        <div className="flex flex-col items-center pt-6 gap-4 xl:gap-3 xl:flex-row xl:flex ">
           <div className=" uppercase rounded-[75px] border-zinc-800 border-2 border-radiu w-52 h-16  flex flex-col justify-center items-center  ">
             <p className="font-StapelLight">status</p>
             <p> 🟢 active</p>
