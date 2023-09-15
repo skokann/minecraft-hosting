cb413418857464d353bdc8862ea3350c203950b3
Jiří Zubryckyj <64204363+skokann@users.noreply.github.com>
2023-09-15 22:54:52 +0200

    better mobile ver

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,5 +1,21 @@
 function Bar() {
-  return <div className="w-full h-[140px] bg-green-500"></div>;
+  return (
+    <div className="w-full h-[140px] bg-green-500 flex flex-row">
+      <div>
+        <p>02:15:25</p>
+        <p>DAYS HOURS MINUTES</p>
+      </div>
+      <div>
+        <p>
+          Looking for the perfect home for your Minecraft adventures? NOW WITH
+          15% off
+        </p>
+      </div>
+      <div>
+        <p>GET YOUR SERVER NOW</p>
+      </div>
+    </div>
+  );
 }
 
 export default Bar;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,16 +10,16 @@ function Hero() {
           Your Ultimate Minecraft Hosting <br /> Solution
         </h2>
         <div className="flex flex-col items-center pt-6 gap-4 xl:gap-3 xl:flex-row xl:flex ">
-          <div className=" uppercase rounded-[75px] border-zinc-800 border-2 border-radiu w-52 h-16  flex flex-col justify-center items-center  ">
-            <p className="font-StapelLight">status</p>
+          <div className=" uppercase rounded-[75px] border-zinc-800 border-2 border-radiu w-52 h-[4.5rem]  flex flex-col justify-center items-center  ">
+            <p className="font-StapelLight text-gray-500">status</p>
             <p> 🟢 active</p>
           </div>
-          <div className=" uppercase rounded-[75px] border-zinc-800  border-2 w-52 h-16 flex flex-col justify-center items-center">
-            <p className="">online players</p>
+          <div className=" uppercase rounded-[75px] border-zinc-800  border-2 w-52 h-[4.5rem] flex flex-col justify-center items-center">
+            <p className="text-gray-500">online players</p>
             <p>4,688</p>
           </div>
-          <div className="uppercase rounded-[75px] border-zinc-800  border-2 w-52 h-16 flex flex-col justify-center items-center">
-            <p className="">online servers</p>
+          <div className="uppercase rounded-[75px] border-zinc-800  border-2 w-52 h-[4.5rem] flex flex-col justify-center items-center">
+            <p className="text-gray-500">online servers</p>
             <p>1,256</p>
           </div>
         </div>
